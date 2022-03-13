d2ab4b0db976b83e704b9b9abb0d4085f35767f3
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-03-13 11:45:16 +0000

    add express lab06

---
+++ lab01/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const hbs = require('hbs')
+const wax = require('wax-on');
+
+let app = express()
+
+// 1B. SETUP VIEW ENGINE
+app.set('view engine', 'hbs'); 
+
+// 1C. SETUP STATIC FOLDER
+app.use(express.static('public'));
+
+// 1D. SETUP WAX ON (FOR TEMPLATE INHERITANCE)
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts')
+
+// add routes here 
+
+// app.get('/', function(req,res){
+//     res.send("<h1>Hello from Express</h1>")
+// })
+
+app.get('/',function(req, res){
+    res.render('index.hbs')
+})
+
+// app.get('/hello/:name', (req,res)=>{
+//     let name= req.params.name;
+//     res.send("Hi," + name);
+// })
+
+app.listen(3000,()=>{
+    console.log("Server started")
+})
